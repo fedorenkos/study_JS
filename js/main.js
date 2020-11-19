7b9e981d47aa3693ae7e129d42dfdf9faa74febf
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-11-19 18:05:09 +0200

    Cделал ДЗ, добавил переменные в js и прописал alert и console.log

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ let message = function() {
 };
 
 document.getElementById('github').addEventListener('click', message);
+
+
+const money = 0;
+const income = 'a lot';
+const addExpenses = 333;
+const deposit = true;
+const mission = 'to earn a lot mony';
+const period = '6 months';
+
+alert('Hello World');
+console.log('Good Bye World');

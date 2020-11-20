8163622c85dd5334631d3ecc9f8c4c1767ff4898
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-11-20 15:33:07 +0200

    Сделана домашка по второму уроку

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
-let message = function() {
-    alert('Hello');
-};
+// let message = function() {
+//     alert('Hello');
+// };
 
-document.getElementById('github').addEventListener('click', message);
+// document.getElementById('github').addEventListener('click', message);
 
 
-const money = 0;
-const income = 'a lot';
-const addExpenses = 333;
+const money = 50000;
+const income = 'freelance';
+const addExpenses = 'Internet, Taxi, Taxes, Food, Clothes, Travelling';
 const deposit = true;
-const mission = 'to earn a lot mony';
-const period = '6 months';
+const mission = 1000000;
+const period = 12;
 
-alert('Hello World');
-console.log('Good Bye World');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' $');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+const budgetDay = 30;
+console.log('Доход за месяц ' + budgetDay + '$');

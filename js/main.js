67b59bfa11b943c6fde91d1c988b566439af8bbb
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-11-23 17:55:59 +0200

    Выполнил 5 заданий ДЗ-3

--- js/main.js
+++ js/main.js
@@ -26,3 +26,28 @@ console.log(addExpenses.split(', '));
 
 const budgetDay = (money / 30);
 console.log('Доход за месяц ' + budgetDay + '$');
+
+console.log("----------------------ДЗ-3----------------------");
+
+let question = prompt('Ваш месячный доход?', money);
+
+console.log(typeof(question));
+
+let question1 = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', addExpenses);
+
+console.log(typeof(question1));
+
+let question2 = confirm('Есть ли у вас депозит в банке?', deposit);
+
+console.log(typeof(question2));
+
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+console.log(expenses1);
+let expenses2 = prompt('Введите обязательную статью расходов?');
+console.log(expenses2);
+let amount1 = prompt('Во сколько это обойдется?');
+console.log(amount1);
+let amount2 = prompt('Во сколько это обойдется?');
+console.log(amount2);
+// 5 заданий

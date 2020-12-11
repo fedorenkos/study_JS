ab223b5dbbb2d493e75edf4801395958a7ace61b
Serg <fedorenkos084@gmail.com>
2020-12-11 09:17:52 +0200

    вывел возможные расходы (addExpenses) вывести строкой в консоль

--- js/main.js
+++ js/main.js
@@ -55,10 +55,11 @@ let appData = {
         let upperCaseWord = [];
 
         for (let i = 0; i < words.length; i++) {
-            let word = words[i].trim();
-                upperCaseWord.push(word[0].toUpperCase() + word.slice(1));
+        let word = words[i].trim();
+        upperCaseWord.push(word[0].toUpperCase() + word.slice(1));
         }
-        console.log(upperCaseWord);
+        console.log(upperCaseWord.join(', '));
+                
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
 
 
